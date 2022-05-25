e525724c5cbd45e7876c6620cf56ffb40fecdeab
Jess Zarchi <jackfz01@gmail.com>
2022-05-25 12:23:38 -0700

    added brake mode, moving deadzone, turn curve

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,7 @@ pros::Motor l2_back(18);
 pros::Motor r1_back(15, true);
 pros::Motor r2_back(16);
 
-pros::Motor flywheel (4, MOTOR_GEARSET_06, false, MOTOR_ENCODER_DEGREES);
+pros::Motor flywheel(4, MOTOR_GEARSET_06, false, MOTOR_ENCODER_DEGREES);
 pros::Motor intake(3);
 
 void set_drive(int fl, int bl, int fr, int br) {
@@ -25,6 +25,17 @@ void set_drive(int fl, int bl, int fr, int br) {
   r2_back = br;
 }
 
+void drive_brake(pros::motor_brake_mode_e_t input) {
+  l1_front.set_brake_mode(input);
+  l2_front.set_brake_mode(input);
+  r1_front.set_brake_mode(input);
+  r2_front.set_brake_mode(input);
+  l1_back.set_brake_mode(input);
+  l2_back.set_brake_mode(input);
+  r1_back.set_brake_mode(input);
+  r2_back.set_brake_mode(input);
+}
+
 void set_flywheel(int input) {
   flywheel = input;
 }
@@ -33,8 +44,6 @@ void set_intake(int input) {
   intake = input;
 }
 
-
-
 /**
  * Runs initialization code. This occurs as soon as the program is started.
  *
@@ -42,7 +51,7 @@ void set_intake(int input) {
  * to keep execution time for this mode under a few seconds.
  */
 void initialize() {
-	pros::lcd::initialize();
+  pros::lcd::initialize();
 }
 
 /**
@@ -92,80 +101,95 @@ void autonomous() {}
 
 double targetRPM = 0;
 double getRPM() {
-  return flywheel.get_actual_velocity()*5;
+  return flywheel.get_actual_velocity() * 5;
 }
 bool flywheel_on = false;
 void flywheel_control() {
-     const double kP = 3.0;
+  const double kP = 3.0;
 
-     const double maxVoltage = 12000;
+  const double maxVoltage = 12000;
 
-     // Geared for 3000RPM (600 5:1), but this is the measured max RPM
-     const double maxRPM = 3200;
+  // Geared for 3000RPM (600 5:1), but this is the measured max RPM
+  const double maxRPM = 3200;
 
-     // Multiplied by a scalar to adjust for the real world
-     const double kv = 0.98 * maxVoltage / maxRPM;
+  // Multiplied by a scalar to adjust for the real world
+  const double kv = 0.98 * maxVoltage / maxRPM;
 
-     // "Acceptable" velocity range to switch to hold power + p control
-     double deadband = 100;
+  // "Acceptable" velocity range to switch to hold power + p control
+  double deadband = 100;
 
-     while (1) {
-         //rpmGuard.take(2);
+  while (1) {
+    // rpmGuard.take(2);
 
+    double rpmError = targetRPM - getRPM();
 
-         double rpmError = targetRPM - getRPM();
+    double holdPower = targetRPM * kv;
 
-         double holdPower = targetRPM * kv;
+    double output = 0;
 
-         double output = 0;
+    // Coast down
+    if (targetRPM == 0) {
+      pros::lcd::set_text(2, "Flywheel Off!         ");
+      output = 0;
+    }
+    // Running too slow (< target - deadband), apply full power
+    else if (rpmError > deadband) {
+      pros::lcd::set_text(2, "Flywheel Too Slow!      ");
+      output = maxVoltage;
+    }
+    // Running too fast (> target + deadband), apply half of hold power
+    else if (rpmError < -deadband) {
+      pros::lcd::set_text(2, "Flywheel Too Fast!      ");
+      output = holdPower * 0.5;
+    }
+    // Running within deadband, switch to hold power + p contorl
+    else {
+      output = holdPower + kP * rpmError;
+      pros::lcd::set_text(2, "At Velocity!          ");
+    }
 
-         // Coast down
-         if (targetRPM == 0) {
-           pros::lcd::set_text(2, "Flywheel Off!         ");
-             output = 0;
-           }
-         // Running too slow (< target - deadband), apply full power
-         else if (rpmError > deadband) {
-           pros::lcd::set_text(2, "Flywheel Too Slow!      ");
-             output = maxVoltage;
-           }
-         // Running too fast (> target + deadband), apply half of hold power
-         else if (rpmError < -deadband) {
-           pros::lcd::set_text(2, "Flywheel Too Fast!      ");
-             output = holdPower * 0.5;
-           }
-         // Running within deadband, switch to hold power + p contorl
-         else {
-             output = holdPower + kP * rpmError;
-             pros::lcd::set_text(2, "At Velocity!          ");
-           }
+    if (targetRPM >= 0) {
+      // flywheelMotor.moveVoltage(output);
+      // set_flywheel(output);
+      flywheel.move_voltage(output);
+      // lastCommandedVoltage = 0;
+    }
 
-         if (targetRPM >= 0) {
-             //flywheelMotor.moveVoltage(output);
-             //set_flywheel(output);
-             flywheel.move_voltage(output);
-             //lastCommandedVoltage = 0;
-         }
+    // rpmGuard.give();
+    // delay(sampleTime);
+    std::string str_rpm = std::to_string(targetRPM);
+    std::string str_cur = std::to_string((int)getRPM());
+    pros::lcd::set_text(0, "Target Vel: " + str_rpm);
+    pros::lcd::set_text(1, "Current Vel: " + str_cur);
+    pros::delay(10);
+  }
+}
+pros::Task flywheelControl(flywheel_control);
 
-         //rpmGuard.give();
-       //delay(sampleTime);
-       std::string str_rpm = std::to_string(targetRPM);
-       std::string str_cur = std::to_string((int)getRPM());
-       pros::lcd::set_text(0, "Target Vel: " + str_rpm);
-       pros::lcd::set_text(1, "Current Vel: " + str_cur);
-       pros::delay(10);
-     }
- }
- pros::Task flywheelControl(flywheel_control);
+const int initial_deadzone = 5;
+const int final_deadzone = 10;
+double m = (final_deadzone - initial_deadzone) / (127.0);
+int deadzone(int input) {
+  double y = m * abs(input) + initial_deadzone;
+  if (abs(input) > y)
+    return input;
+  return 0;
+}
 
+double inputcurve(int x){
+  float e = 2.718;
+  float t = 2.1;
+  //return (e-(t/10)+e(abs9(x)-127)
+  return (powf(e, -(t/10))+powf(e, ((abs(x)-100)/10))*(1-powf(e, -(t/10))))*x;
+}
 
 void opcontrol() {
+  drive_brake(MOTOR_BRAKE_BRAKE);
 
-	while (true) {
-
-    int Ch3 = master.get_analog(ANALOG_LEFT_Y);
-    int Ch4 = master.get_analog(ANALOG_LEFT_X);
-    int Ch1 = master.get_analog(ANALOG_RIGHT_X);
+  while (true) {
+    int Ch3 = deadzone(master.get_analog(ANALOG_LEFT_Y));
+    int Ch4 = deadzone(master.get_analog(ANALOG_LEFT_X));
+    int Ch1 = inputcurve(deadzone(master.get_analog(ANALOG_RIGHT_X)));
 
     int FrontLeft = Ch3 + Ch1 + Ch4;
     int RearLeft = Ch3 + Ch1 - Ch4;
@@ -173,18 +197,15 @@ void opcontrol() {
     int RearRight = Ch3 - Ch1 + Ch4;
 
     set_drive(FrontLeft, RearLeft, FrontRight, RearRight);
-
-
+    
 
     if (master.get_digital_new_press(DIGITAL_L1)) {
       targetRPM = targetRPM != 0 ? 0 : 3230;
-    }
-    else if (master.get_digital_new_press(DIGITAL_L2)) {
+    } else if (master.get_digital_new_press(DIGITAL_L2)) {
       targetRPM = targetRPM != 0 ? 0 : 3000;
     } else if (master.get_digital_new_press(DIGITAL_UP)) {
-       targetRPM = targetRPM != 0 ? 0 : 2800;
-     }
-
+      targetRPM = targetRPM != 0 ? 0 : 2800;
+    }
 
     if (master.get_digital(DIGITAL_R1)) {
       set_intake(127);
@@ -194,7 +215,6 @@ void opcontrol() {
       set_intake(0);
     }
 
-
-		pros::delay(10);
-	}
+    pros::delay(10);
+  }
 }
