2b221d92e5e9cf5e1976db2fe05a3e7516737f12
Unionjackjz1 <jackfz01@gmail.com>
2022-06-04 22:43:44 -0700

    reset amount of fires just to be sure

--- src/indexer.cpp
+++ src/indexer.cpp
@@ -26,7 +26,10 @@ void indexer_control() {
       set_indexer_piston(false);
       if (timer >= active_time + deactive_time) {
         amount_of_fires--;
-        if (amount_of_fires == 0) indexer_on = false;
+        if (amount_of_fires == 0) {
+          indexer_on = false;
+          amount_of_fires = 0;
+        }
         timer = 0;
       }
       timer += DELAY_TIME;
